7f83de753b2082b528626b103876e1c037495b09
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-25 16:00:36 -0500

    completed challenge 4

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -52,3 +52,22 @@ const ecarTotalWeight = cars.reduce((acc, car) => {
     }
 }, 0);
 // console.log(`Total electric car weight is: ${ecarTotalWeight}`);
+
+// Challenge 4: 
+// In our restaurant, the chef has some favourite dishes in two different categories.
+// The chef loves all dishes that start with "S", while the rest are regular dishes
+// Use array destructoring to create arrays of the chefs favourite dishes of meat and
+// fish, and to create arrays of the regular meat and fish dishes
+
+const fishDishes = ['Salmon Rillettes', 'Grilled Tuna Provencal', 'Fish and Chips']
+const meatDishes = ['Lasagna', 'Spaghetti', 'Satay Chicken Skewers']
+
+// Modify these four variables first
+let [chefsFishDishes, ...regularFishDishes] = fishDishes;
+let [regularMeatDishes, ...chefsMeatDishes] = meatDishes;
+
+// Finally, use the spread operator to create these two arrays as well
+let chefsDishes = [chefsFishDishes, ...chefsMeatDishes];
+let regularDishes = [regularMeatDishes, ...regularFishDishes];
+// console.log(chefsDishes);
+// console.log(regularDishes);
