5cb00b1dc8f4708487f48757bc57e426badf1506
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-25 11:20:41 -0500

    completed challenge 3

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -22,5 +22,33 @@ const songs = [
     {song: "Closer", timesStreamed: 1.688, wonGrammy: false},
     {song: "Thinking Out Loud", timesStreamed: 1.461, wonGrammy: true}
 ]
-console.log(songs.some(song => song.wonGrammy));
-console.log(songs.every(song => song.timesStreamed > 1));
+// console.log(songs.some(song => song.wonGrammy));
+// console.log(songs.every(song => song.timesStreamed > 1.5));
+
+// Challenge 3: 
+// Use reduce() to sum the weights of all the cars
+// Stretch goal: 
+// Use reduce to sum the weights of only the electric cars 
+
+const cars = [
+    { name: "Toyota", isElectric: false, weight: 1320 },
+    { name: "Ford", isElectric: false, weight: 1400 },
+    { name: "Volkswagen", isElectric: false, weight: 1370 },
+    { name: "Honda", isElectric: false, weight: 1375 },
+    { name: "Tesla", isElectric: true, weight: 1750 },
+    { name: "BMW", isElectric: true, weight: 1350 },  
+  ];
+
+const totalWeight = cars.reduce((acc, car) => {
+    return acc + car.weight;
+}, 0);
+// console.log(`Total weight of cars is: ${totalWeight}`);
+
+const ecarTotalWeight = cars.reduce((acc, car) => {
+    if (car.isElectric) {
+        return acc + car.weight;
+    } else {
+        return acc
+    }
+}, 0);
+// console.log(`Total electric car weight is: ${ecarTotalWeight}`);
