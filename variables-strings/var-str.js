b2739f1b40f1c845d5e8ddc4f8ebe6b1f8d66425
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-27 20:13:38 -0500

    completed challenge 1

---
+++ variables-strings/var-str.js
@@ -0,0 +1,8 @@
+// Fix the variable names below, and log them out to the console!
+
+const firstname = "Reed";
+console.log(firstname);
+const surName = "Barger";
+console.log(surName);
+let oneThousand = "one thousand";
+console.log(oneThousand);
