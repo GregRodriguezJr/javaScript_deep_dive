68dd504892ebbe451c201fe5d522e5068337c68b
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-27 20:18:07 -0500

    completed challenge 2,3,4

--- variables-strings/var-str.js
+++ variables-strings/var-str.js
@@ -6,3 +6,19 @@ const surName = "Barger";
 console.log(surName);
 let oneThousand = "one thousand";
 console.log(oneThousand);
+
+// Challenge 2: We want to log out the name "Reed", but it doesn't work. Can you fix it?
+let name;
+name = "Reed";
+console.log(name);
+
+// Challenge 3: We want 102 to be logged out. But we're getting an error instead. Try to fix it!
+let count = 100;
+let = 101;
+let = 102;
+console.log(count);
+
+//  Challenge 4: We want to log out the price (50), but it doesn't work. Can you fix it?
+let price;
+price = 50;
+console.log(price);
